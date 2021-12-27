cfd3a25c753f6cdcbecba53f241953f9099d02b8
mohammedBensaad <bensaadmucret@gmail.com>
2021-12-27 17:47:31 +0100

    add css

--- Layouts/default.php
+++ Layouts/default.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/main.css'); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <title>Non !</title>
 </head>
 <body>
@@ -14,13 +16,16 @@
      <?php echo  $content; ?>
     </div>
    <div class="sidenav">
+     <a href="<?php echo absolutePath('/'); ?>"><i class="fas fa-cloud"></i></a>  
     <a href="<?php echo absolutePath('/'); ?>">Home</a>   
     <a href="<?php echo absolutePath('/about'); ?>">About</a>
     <a href="<?php echo absolutePath('/blog'); ?>">Blog</a>
     <a href="<?php echo absolutePath('/contact'); ?>">Contact</a>
+    <button class="btn"> Promos !</button>   
+    <button class="btn pannier"> Pannier </button> 
     
-   
     </div>
+   
 </div>
 
 </body>

35374d0028fab0ec13cea0208a54bd918c6fbb6f
mohammedBensaad <bensaadmucret@gmail.com>
2022-01-10 15:27:00 +0100

    structure html and css

--- Layouts/default.php
+++ Layouts/default.php
@@ -1,32 +1,228 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <meta name="description" content="">
-    <link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/style.css'); ?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <title>Non !</title>
-</head>
-<body>
-<div class="main">
-    <div class="container">
-     <?php echo  $content; ?>
-    </div>
-   <div class="sidenav">
-    <a class="sidenav_item" href="<?php echo absolutePath('/'); ?>"><i class="fas fa-cloud" style="font-size:60px;color:lightblue;"></i></a>  
-    <a class="sidenav_item" href="<?php echo absolutePath('/'); ?>">Home</a>   
-    <a class="sidenav_item" href="<?php echo absolutePath('/about'); ?>">About</a>
-    <a class="sidenav_item" href="<?php echo absolutePath('/blog'); ?>">Blog</a>
-    <a class="sidenav_item" href="<?php echo absolutePath('/contact'); ?>">Contact</a>
-    <button class="btn sidenav_item"> Promos !</button>   
-    <button class="btn pannier sidenav_item"> Pannier </button> 
-    
-    </div>
-   
-</div>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/main_styles.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/categories_responsive.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/responsive.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/contact_responsive.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/contact_styles.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/single_responsive.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/single_styles.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css'); ?>" >
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/plugins/OwlCarousel2-2.2.1/owl.carousel.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/plugins/OwlCarousel2-2.2.1/owl.theme.default.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/plugins/OwlCarousel2-2.2.1/animate.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/plugins/themify-icons/themifly-icons.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo absolutePath('/assets/css/responsive.css'); ?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0/css/bootstrap-grid.min.css">
 
+
+</head>
+
+<body>
+
+<div class="super_container">
+
+	<!-- Header -->
+
+	<header class="header trans_300">
+
+		<!-- Top Navigation -->
+
+		<div class="top_nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="top_nav_left">free shipping on all u.s orders over $50</div>
+					</div>
+					<div class="col-md-6 text-right">
+						<div class="top_nav_right">
+							<ul class="top_nav_menu">
+
+								<!-- Currency / Language / My Account -->
+
+								<li class="currency">
+									<a href="#">
+										usd
+										<i class="fa fa-angle-down"></i>
+									</a>
+									<ul class="currency_selection">
+										<li><a href="#">cad</a></li>
+										<li><a href="#">aud</a></li>
+										<li><a href="#">eur</a></li>
+										<li><a href="#">gbp</a></li>
+									</ul>
+								</li>
+								<li class="language">
+									<a href="#">
+										English
+										<i class="fa fa-angle-down"></i>
+									</a>
+									<ul class="language_selection">
+										<li><a href="#">French</a></li>
+										<li><a href="#">Italian</a></li>
+										<li><a href="#">German</a></li>
+										<li><a href="#">Spanish</a></li>
+									</ul>
+								</li>
+								<li class="account">
+									<a href="#">
+										My Account
+										<i class="fa fa-angle-down"></i>
+									</a>
+									<ul class="account_selection">
+										<li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
+										<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Main Navigation -->
+
+		<div class="main_nav_container">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-right">
+						<div class="logo_container">
+							<a href="#">My<span>shop</span></a>
+						</div>
+						<nav class="navbar">
+							<ul class="navbar_menu">
+								<li><a href="#">home</a></li>
+								<li><a href="#">shop</a></li>
+								<li><a href="#">promotion</a></li>
+								<li><a href="#">pages</a></li>
+								<li><a href="#">blog</a></li>
+								<li><a href="contact.html">contact</a></li>
+							</ul>
+							<ul class="navbar_user">
+								<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+								<li class="checkout">
+									<a href="#">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										<span id="checkout_items" class="checkout_items">2</span>
+									</a>
+								</li>
+							</ul>
+							<div class="hamburger_container">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</header>
+
+	<div class="fs_menu_overlay"></div>
+	<div class="hamburger_menu">
+		<div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
+		<div class="hamburger_menu_content text-right">
+			<ul class="menu_top_nav">
+				<li class="menu_item has-children">
+					<a href="#">
+						usd
+						<i class="fa fa-angle-down"></i>
+					</a>
+					<ul class="menu_selection">
+						<li><a href="#">cad</a></li>
+						<li><a href="#">aud</a></li>
+						<li><a href="#">eur</a></li>
+						<li><a href="#">gbp</a></li>
+					</ul>
+				</li>
+				<li class="menu_item has-children">
+					<a href="#">
+						English
+						<i class="fa fa-angle-down"></i>
+					</a>
+					<ul class="menu_selection">
+						<li><a href="#">French</a></li>
+						<li><a href="#">Italian</a></li>
+						<li><a href="#">German</a></li>
+						<li><a href="#">Spanish</a></li>
+					</ul>
+				</li>
+				<li class="menu_item has-children">
+					<a href="#">
+						My Account
+						<i class="fa fa-angle-down"></i>
+					</a>
+					<ul class="menu_selection">
+						<li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
+						<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
+					</ul>
+				</li>
+				<li class="menu_item"><a href="#">home</a></li>
+				<li class="menu_item"><a href="#">shop</a></li>
+				<li class="menu_item"><a href="#">promotion</a></li>
+				<li class="menu_item"><a href="#">pages</a></li>
+				<li class="menu_item"><a href="#">blog</a></li>
+				<li class="menu_item"><a href="#">contact</a></li>
+			</ul>
+		</div>
+	</div>
+
+
+
+    <?php echo $content; ?>
+	<!-- Footer -->
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
+						<ul class="footer_nav">
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">FAQs</a></li>
+							<li><a href="contact.html">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="footer_nav_container">
+						<div class="cr">©2018 All Rights Reserverd. This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="<?php echo absolutePath('/assets/js/jquery-3.2.1.min.js'); ?>"></script>
+<script src="<?php echo absolutePath('/assets/styles/bootstrap4/popper.js'); ?>"></script>
+<script src="<?php echo absolutePath('/assets/styles/bootstrap4/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo absolutePath('/assets/plugins/Isotope/isotope.pkgd.min.js'); ?>"></script>
+<script src="<?php echo absolutePath('/assets/plugins/OwlCarousel2-2.2.1/owl.carousel.js'); ?>"></script>
+<script src="<?php echo absolutePath('/assets/plugins/easing/easing.js'); ?>"></script>
+<script src="<?php echo absolutePath('/assets/js/custom.js'); ?>"></script>
 </body>
+
 </html>
--- Layouts/home/index.php
+++ Layouts/home/index.php
@@ -1,4 +1,56 @@
+	<!-- Slider -->
 
-
-
+	<div class="main_slider" style="background-image:url(<?php echo absolutePath('/assets/images/slider_1.jpg'); ?> )">
+		<div class="container fill_height">
+			<div class="row align-items-center fill_height">
+				<div class="col">
+					<div class="main_slider_content">
+						<h6>Spring / Summer Collection 2017</h6>
+						<h1>Get up to 30% Off New Arrivals</h1>
+						<div class="red_button shop_now_button"><a href="#">shop now</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
   
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ">
+                <h1 class="m-5 text-center">Welcome to the home page</h1>
+                <p class="m-5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, quisquam, doloremque, ipsum, quaerat, debitis, voluptates, tempora, eius, vero, quibusdam. Quasi, dolorem, quis, voluptate, quam, quisquam, doloremque, quae, iste, dolores, voluptas, quam!</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+	<!-- Banner -->
+
+    <div class="banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="banner_item align-items-center" style="background-image:url(<?php echo absolutePath('/assets/images/banner_1.jpg'); ?>')">
+						<div class="banner_category">
+							<a href="categories.html">women's</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="banner_item align-items-center" style="background-image:url(<?php echo absolutePath('/assets/images/banner_2.jpg');?>)">
+						<div class="banner_category">
+							<a href="categories.html">accessories's</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="banner_item align-items-center" style="background-image:url(<?php echo absolutePath('/assets/images/banner_3.jpg')?>)">
+						<div class="banner_category">
+							<a href="categories.html">men's</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>  
